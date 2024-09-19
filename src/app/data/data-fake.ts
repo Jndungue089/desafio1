a2fac6b7eae642c45b31730ad501d76618d11803
Jndungue089 <68353090+Jndungue089@users.noreply.github.com>
2024-09-19 10:23:52 +0100

    Content Created

--- src/app/data/data-fake.ts
+++ src/app/data/data-fake.ts
@@ -1,14 +1,26 @@
 export const DataFake = [
     {
-        "id":1,
-        "image": "aaa",
-        "title": "angular 17+",
-        "description": "ola"
+        "id": "1",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQihN_IRIXwi515XwNAEmtKw4vAmoUzoPJ7KA&s",
+        "title": "Saiu nova versão do Angular!",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dui risus, rutrum condimentum malesuada nec, mollis sed risus. Duis eget feugiat metus, vel maximus lorem. Praesent pellentesque consectetur ligula quis aliquam. Nunc efficitur enim."
     },
     {
-        "id":2,
-        "image": "aaa",
-        "title": "angular 17+",
-        "description": "ola"
-    }
+        "id": "2",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQihN_IRIXwi515XwNAEmtKw4vAmoUzoPJ7KA&s",
+        "title": "Angular Material: é bom mesmo?",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dui risus, rutrum condimentum malesuada nec, mollis sed risus. Duis eget feugiat metus, vel maximus lorem. Praesent pellentesque consectetur ligula quis aliquam. Nunc efficitur enim."
+    },
+    {
+        "id": "3",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQihN_IRIXwi515XwNAEmtKw4vAmoUzoPJ7KA&s",
+        "title": "Vamos ver os Buttons do Angular Material",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dui risus, rutrum condimentum malesuada nec, mollis sed risus. Duis eget feugiat metus, vel maximus lorem. Praesent pellentesque consectetur ligula quis aliquam. Nunc efficitur enim."
+    },
+    {
+        "id": "4",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQihN_IRIXwi515XwNAEmtKw4vAmoUzoPJ7KA&s",
+        "title": "Não sei mais o quê",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dui risus, rutrum condimentum malesuada nec, mollis sed risus. Duis eget feugiat metus, vel maximus lorem. Praesent pellentesque consectetur ligula quis aliquam. Nunc efficitur enim."
+    },
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataFake } from '../../data/data-fake';
+
 
 @Component({
   selector: 'app-content',
@@ -8,9 +11,29 @@ import { Component } from '@angular/core';
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
-  image:string =""
-  title:string =""
-  description:string =""
-  constructor(){}
+  image: string = ""
+  title: string = ""
+  description: string = ""
+  private id: string|any = "0"
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get('id')
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id: string) {
+    const result = DataFake.filter(article => article.id == id)[0]
+    
+    this.title = result.title
+    this.description = result.description
+    this.image = result.image
+  }
+
 
 }
